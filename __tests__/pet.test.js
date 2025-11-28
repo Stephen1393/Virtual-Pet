accd337776954e45590bab7858a29269e02e3f77
Stephen1393 <stephen.bending20@gmail.com>
2025-11-28 15:29:47 +0000

    Adding petname in function and test file.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -2,6 +2,12 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
   it('returns an object', () => {
-    expect(new Pet('Fido')).toBeInstanceOf(Object);
+    expect(new Pet('Stanley')).toBeInstanceOf(Object);
+
   });
-});
+
+    it('returns the name', () =>  {
+      const pet = new Pet("Stanley");
+      expect(pet.name).toBe("Stanley");
+    });
+  });
--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,6 @@
-function Pet() {}
+function Pet(name) {
+    this.name = name
+}
+const myPet = Pet("Stanley")
 
 module.exports = Pet;
